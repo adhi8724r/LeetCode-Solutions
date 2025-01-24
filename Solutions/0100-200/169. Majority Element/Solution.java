fd88c1069aef21652d0532ecf80c9c8e36a1cbfd
adi sai kiran <saikiranadhi15@gmail.com>
2025-01-24 17:26:03 +0530

    Create Solution.java

---
+++ Solutions/0100-200/169. Majority Element/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate=0;
+        int count=0;
+        for(int num: nums)
+        {
+            if(count==0)
+            {
+                candidate=num;
+                count+=1;
+            }
+            else if(candidate==num)
+            {
+                count+=1;
+            }
+            else
+            {
+                count-=1;
+            }
+        }
+        return candidate;
+    }
+}
